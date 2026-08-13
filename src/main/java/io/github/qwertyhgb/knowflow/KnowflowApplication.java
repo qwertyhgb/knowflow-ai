a2692485b1ef2b3ba3a91be45107702018968b68
lm <2168884970@qq.com>
2026-08-13 09:56:57 +0800

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/qwertyhgb/knowflow/KnowflowApplication.java
@@ -0,0 +1,13 @@
+package io.github.qwertyhgb.knowflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/qwertyhgb/knowflow/KnowflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.qwertyhgb.knowflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
